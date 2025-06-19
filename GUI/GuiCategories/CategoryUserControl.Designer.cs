e52486ae9f0e4b03c6a56c631d50f3c61924ac9c
MohammedFares <xmohammedfares@gmail.com>
2025-06-19 16:18:01 +0300

    Add Category User Control

---
+++ GUI/GuiCategories/CategoryUserControl.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ASRFLY.Desktop.GUI.GuiCategories
+{
+    partial class CategoryUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            btnExport = new Button();
+            panel1 = new Panel();
+            txtSearch = new TextBox();
+            btnSearch = new Button();
+            dgvCategories = new DataGridView();
+            flowLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvCategories).BeginInit();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.BackColor = SystemColors.Control;
+            flowLayoutPanel1.Controls.Add(btnAdd);
+            flowLayoutPanel1.Controls.Add(btnEdit);
+            flowLayoutPanel1.Controls.Add(btnDelete);
+            flowLayoutPanel1.Controls.Add(btnExport);
+            flowLayoutPanel1.Controls.Add(panel1);
+            flowLayoutPanel1.Dock = DockStyle.Top;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Padding = new Padding(5);
+            flowLayoutPanel1.Size = new Size(1262, 76);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Image = Properties.Resources.Plus;
+            btnAdd.ImageAlign = ContentAlignment.MiddleLeft;
+            btnAdd.Location = new Point(1112, 10);
+            btnAdd.Margin = new Padding(5);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(135, 55);
+            btnAdd.TabIndex = 2;
+            btnAdd.Text = "اضافة";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Image = Properties.Resources.Edit;
+            btnEdit.ImageAlign = ContentAlignment.MiddleLeft;
+            btnEdit.Location = new Point(967, 10);
+            btnEdit.Margin = new Padding(5);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(135, 55);
+            btnEdit.TabIndex = 3;
+            btnEdit.Text = "تعديل";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Image = Properties.Resources.Delete;
+            btnDelete.ImageAlign = ContentAlignment.MiddleLeft;
+            btnDelete.Location = new Point(822, 10);
+            btnDelete.Margin = new Padding(5);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(135, 55);
+            btnDelete.TabIndex = 4;
+            btnDelete.Text = "حذف";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnExport
+            // 
+            btnExport.Image = Properties.Resources.Export;
+            btnExport.ImageAlign = ContentAlignment.MiddleLeft;
+            btnExport.Location = new Point(677, 10);
+            btnExport.Margin = new Padding(5);
+            btnExport.Name = "btnExport";
+            btnExport.Size = new Size(135, 55);
+            btnExport.TabIndex = 5;
+            btnExport.Text = "تصدير";
+            btnExport.UseVisualStyleBackColor = true;
+            btnExport.Click += btnExport_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(txtSearch);
+            panel1.Controls.Add(btnSearch);
+            panel1.Location = new Point(181, 8);
+            panel1.Margin = new Padding(3, 3, 5, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(488, 55);
+            panel1.TabIndex = 6;
+            // 
+            // txtSearch
+            // 
+            txtSearch.BorderStyle = BorderStyle.FixedSingle;
+            txtSearch.Dock = DockStyle.Fill;
+            txtSearch.Font = new Font("Cairo", 16F);
+            txtSearch.Location = new Point(0, 0);
+            txtSearch.Margin = new Padding(0);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(395, 57);
+            txtSearch.TabIndex = 8;
+            txtSearch.TextAlign = HorizontalAlignment.Center;
+            txtSearch.TextChanged += txtSearch_TextChanged;
+            // 
+            // btnSearch
+            // 
+            btnSearch.BackColor = Color.DodgerBlue;
+            btnSearch.Dock = DockStyle.Right;
+            btnSearch.FlatAppearance.BorderSize = 0;
+            btnSearch.FlatStyle = FlatStyle.Flat;
+            btnSearch.Font = new Font("Cairo", 12F, FontStyle.Bold);
+            btnSearch.ForeColor = Color.White;
+            btnSearch.Image = Properties.Resources.Search_white;
+            btnSearch.ImageAlign = ContentAlignment.MiddleLeft;
+            btnSearch.Location = new Point(395, 0);
+            btnSearch.Margin = new Padding(5);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(93, 55);
+            btnSearch.TabIndex = 7;
+            btnSearch.Text = "     بحث";
+            btnSearch.UseVisualStyleBackColor = false;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // dgvCategories
+            // 
+            dgvCategories.AllowUserToDeleteRows = false;
+            dgvCategories.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvCategories.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvCategories.BackgroundColor = Color.White;
+            dgvCategories.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Cairo", 12F);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            dgvCategories.DefaultCellStyle = dataGridViewCellStyle1;
+            dgvCategories.Dock = DockStyle.Fill;
+            dgvCategories.GridColor = Color.Black;
+            dgvCategories.Location = new Point(0, 76);
+            dgvCategories.Name = "dgvCategories";
+            dgvCategories.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Cairo", 12F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dgvCategories.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dgvCategories.RowHeadersWidth = 51;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvCategories.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            dgvCategories.Size = new Size(1262, 524);
+            dgvCategories.TabIndex = 1;
+            dgvCategories.CellDoubleClick += dgvCategories_CellDoubleClick;
+            // 
+            // CategoryUserControl
+            // 
+            AutoScaleDimensions = new SizeF(11F, 37F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(dgvCategories);
+            Controls.Add(flowLayoutPanel1);
+            Font = new Font("Cairo", 12F);
+            Margin = new Padding(4, 6, 4, 6);
+            Name = "CategoryUserControl";
+            RightToLeft = RightToLeft.Yes;
+            Size = new Size(1262, 600);
+            flowLayoutPanel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvCategories).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnDelete;
+        private Button btnExport;
+        private Panel panel1;
+        private Button btnSearch;
+        private TextBox txtSearch;
+        private DataGridView dgvCategories;
+    }
+}
